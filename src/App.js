69bd4ab9e3d9ae921968fb46f2e9f7a42e56e6b2
renlei3 <807588313@qq.com>
2018-02-28 17:41:14 +0800

    采用容器组件高阶函数

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/* import React from 'react'
 import BankBalanceStore from './BankBalanceStore'
 import BankActions from './BankActions'
 
@@ -52,4 +52,49 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default App */
+
+// 采用容器组件高阶函数
+import React from 'react'
+import BankBalanceStore from './BankBalanceStore'
+import BankActions from './BankActions'
+import {Container} from 'flux/utils'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    BankActions.createAccount()
+  }
+
+  deposit() {
+    BankActions.depositIntoAccount(Number(this.refs.ammount.value))
+    this.refs.ammount.value = ' '
+  }
+
+  withdraw() {
+    BankActions.withdrawFromAccount(Number(this.refs.ammount.value))
+    this.refs.ammount.value = ' '
+  }
+
+  render() {
+    return (
+      <div>
+        <header>FluxTrust Bank</header>
+        <h1>Your balance is ${(this.state.balance).toFixed(2)}</h1>
+        <div className='atm'>
+          <input type="text" placeholder="Enter Ammount" ref="ammount"/>
+          <div className="container">
+          <button onClick={this.withdraw.bind(this)}>Withdraw</button>
+          <button onClick={this.deposit.bind(this)}>Deposit</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+App.getStores = () => ([BankBalanceStore])
+App.calculateState = (prevState) => (
+  {balance : BankBalanceStore.getState()}
+)
+export default Container.create(App)
